dfe6a22034241f2ced6cac246ffc119bb4aa48fa
BonnieProsper <bonniep.mcconnell@gmail.com>
2026-03-08 14:21:35 +1300

    add batch translation and sourcelang to base

--- vocablens/providers/translation/base.py
+++ vocablens/providers/translation/base.py
@@ -1,8 +1,22 @@
-from typing import Protocol
+from typing import Protocol, List
 
 
 class Translator(Protocol):
-    def translate(self, text: str, target_lang: str) -> str:
+
+    def translate(
+        self,
+        text: str,
+        source_lang: str,
+        target_lang: str,
+    ) -> str:
+        ...
+
+    def translate_batch(
+        self,
+        texts: List[str],
+        source_lang: str,
+        target_lang: str,
+    ) -> List[str]:
         ...
 
     def close(self) -> None:
